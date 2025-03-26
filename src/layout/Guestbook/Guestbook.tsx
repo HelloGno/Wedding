4ed965550c5fe6ce28f03619460e8d906bac1914
HelloGno <128276563+HelloGno@users.noreply.github.com>
2025-03-26 14:18:03 +0900

    Update Guestbook.tsx

--- src/layout/Guestbook/Guestbook.tsx
+++ src/layout/Guestbook/Guestbook.tsx
@@ -6,7 +6,7 @@ const Guestbook = () => {
   return (
     <GuestBookWrapper>
       <Heading2>
-        전하고 싶은 메시지를 남겨주세요!
+        부담없이 전하고 싶은 메시지를 남겨주세요!
         <br />
         신랑 신부만 볼 수 있게 전달됩니다.
       </Heading2>
